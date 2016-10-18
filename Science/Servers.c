066e847d67f0ffcddbc49d0a3a358e812dc3b03d
jjr-simiatec <john.robertson@simiatec.com>
2016-10-18 21:39:36 +0100

    PIC32MX should be able to run the full demo now.
    Added support for ping test.

--- Science/Servers.c
+++ Science/Servers.c
@@ -1,6 +1,6 @@
 /*
  * FreeRTOS+TCP Server Test (mostly from FreeRTOS+TCP demo)
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
  * published by the Free Software Foundation.
@@ -14,38 +14,45 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// RTOS
 #include <FreeRTOS.h>
 #include <task.h>
-
+// TCP/IP Stack
 #include <FreeRTOS_IP.h>
 #include <FreeRTOS_TCP_Server.h>
+// File System
 #include <ff_ramdisk.h>
 
 #include "TestHarness.h"
 
 // The number and size of sectors that will make up the RAM disk
-#define mainRAM_DISK_SECTOR_SIZE        512UL   // Currently fixed!
-#define mainRAM_DISK_SECTORS            ((64UL * 1024UL) / mainRAM_DISK_SECTOR_SIZE)
-#define mainIO_MANAGER_CACHE_SIZE       (4UL * mainRAM_DISK_SECTOR_SIZE)
+#define mainRAM_DISK_SECTOR_SIZE    512UL
 
-#define mainRAM_DISK_NAME               "/ram"
-
+#if defined(__PIC32MX__)
+#define mainRAM_DISK_SECTORS    ((32UL * 1024UL) / mainRAM_DISK_SECTOR_SIZE)
+static const char pBOARD_NAME[] = "pic32mx";
+#elif defined(__PIC32MZ__)
+#define mainRAM_DISK_SECTORS    ((64UL * 1024UL) / mainRAM_DISK_SECTOR_SIZE)
 static const char pBOARD_NAME[] = "pic32mz";
+#endif
+
+#define mainIO_MANAGER_CACHE_SIZE   (4UL * mainRAM_DISK_SECTOR_SIZE)
+
+#define mainRAM_DISK_NAME           "/ram"
 
 static const struct xSERVER_CONFIG xServerConfiguration[] =
 {
-    // Server type,     port number,    backlog,    root dir.
-    {  eSERVER_HTTP,    80,             12,         configHTTP_ROOT },
-    {  eSERVER_FTP,     21,             12,         "" }
+    // Server type,  port number, backlog, root dir.
+    {  eSERVER_HTTP, 80,          12,      configHTTP_ROOT },
+    {  eSERVER_FTP,  21,          12,      ""              }
 };
 
-#if defined(__PIC32MZ__)
 static uint8_t ucRAMDisk[mainRAM_DISK_SECTORS * mainRAM_DISK_SECTOR_SIZE];
-#endif
 
 static FF_Disk_t *pxRAMDisk = NULL;
-extern TaskHandle_t g_hTask2;
-extern TaskHandle_t g_hPacketTask;
+
+ePingReplyStatus_t g_tPingReplyStatus;
+uint16_t g_nPingReplySequence;
 
 extern void vCreateAndVerifyExampleFiles(char *pcMountPath);
 
@@ -60,28 +67,16 @@ void vApplicationIPNetworkEventHook(eIPCallbackEvent_t eNetworkEvent)
 
 void vApplicationPingReplyHook(ePingReplyStatus_t eStatus, uint16_t usIdentifier)
 {
-#ifdef __DEBUG
-    __builtin_software_breakpoint();
-#endif // __DEBUG
+    g_tPingReplyStatus = eStatus;
+    g_nPingReplySequence = usIdentifier;
+
+    xTaskNotifyGive(g_hTask1);
 }
 
 BaseType_t xApplicationDNSQueryHook(const char *pcName)
 {
     return strcasecmp(pcName, pBOARD_NAME) == 0;
 }
-
-#if defined(__PIC32MX__)
-
-portTASK_FUNCTION(Task2, pParams)
-{
-    for( ; ; )
-    {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);  
-        LATDINV = 0x01;
-    }
-}
-
-#else
 
 portTASK_FUNCTION(Task2, pParams)
 {
@@ -90,15 +85,13 @@ portTASK_FUNCTION(Task2, pParams)
     configASSERT(pxRAMDisk);
 
     vCreateAndVerifyExampleFiles(mainRAM_DISK_NAME);
-    
+
     ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-    
+
     TCPServer_t *pServers = FreeRTOS_CreateTCPServer(xServerConfiguration, _countof(xServerConfiguration));
-            
+
     for( ; ; )
     {
         FreeRTOS_TCPServerWork(pServers, pdMS_TO_TICKS(200));
     }
 }
-
-#endif
