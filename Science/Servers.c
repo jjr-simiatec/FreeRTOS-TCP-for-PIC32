b290f9b96ca6e0e088a9924bbb068f14664f85a4
jjr-simiatec <john.robertson@simiatec.com>
2019-07-05 02:43:54 +0100

    An application callback must now be implemented to calculate the TCP sequence number. This implementation is stolen from the FreeRTOS+TCP Windows demo.

--- Science/Servers.c
+++ Science/Servers.c
@@ -26,6 +26,7 @@
 #include "ff_ramdiskex.h"
 #include "TestHarness.h"
 #include "TCPCommandConsole.h"
+#include "HardwareProfile.h"
 
 // The number and size of sectors that will make up the RAM disk
 #define mainDISK_SECTOR_SIZE    512UL
@@ -41,6 +42,9 @@ static const char pBOARD_NAME[] = "pic32mz";
 
 #define mainRAM_DISK_NAME           "/ram"
 #define mainWEB_DISK_NAME           "/www"
+
+#define RAND_MULTIPLIER     0x015A4E35UL
+#define RAND_INCREMENT      1UL
 
 static const struct xSERVER_CONFIG xServerConfiguration[] =
 {
@@ -58,6 +62,8 @@ static uint8_t ucRAMDisk[mainRAM_DISK_SECTORS * mainDISK_SECTOR_SIZE];
 
 static FF_Disk_t *pxRAMDisk = NULL;
 static FF_Disk_t *pxROMDisk = NULL;
+
+static UBaseType_t s_ulNextRand;
 
 ePingReplyStatus_t g_tPingReplyStatus;
 uint16_t g_nPingReplySequence;
@@ -89,10 +95,20 @@ BaseType_t xApplicationDNSQueryHook(const char *pcName)
 
 portTASK_FUNCTION(Task2, pParams)
 {
+#if defined(PIC32_HAS_WORKING_TRNG)
+    extern uint32_t TRNGRead32(void);
+    s_ulNextRand = TRNGRead32();
+#else
+    time_t timeNow;
+    time(&timeNow);
+
+    s_ulNextRand = timeNow;
+#endif
+
     // Create the RAM disk
     pxRAMDisk = FF_RAMDiskInit(mainRAM_DISK_NAME, ucRAMDisk, mainRAM_DISK_SECTORS, mainIO_MANAGER_CACHE_SIZE);
     configASSERT(pxRAMDisk);
-    
+
     pxROMDisk = FF_DiskImageReadOnlyInit(mainWEB_DISK_NAME, _binary_Web_Disk_img_start, (size_t) &_binary_Web_Disk_img_size / mainDISK_SECTOR_SIZE, mainIO_MANAGER_CACHE_SIZE);
     configASSERT(pxROMDisk);
 
@@ -104,11 +120,18 @@ portTASK_FUNCTION(Task2, pParams)
 
     vRegisterSampleCLICommands();
     RegisterTestHarnessCLICommands();
-    
+
     vStartTCPCommandInterpreterTask(configMINIMAL_STACK_SIZE * 2, 12345, tskIDLE_PRIORITY + 1);
 
     for( ; ; )
     {
         FreeRTOS_TCPServerWork(pServers, pdMS_TO_TICKS(200));
     }
+}
+
+uint32_t ulApplicationGetNextSequenceNumber(uint32_t ulSourceAddress, uint16_t usSourcePort,
+                                            uint32_t ulDestinationAddress, uint16_t usDestinationPort )
+{
+	s_ulNextRand = (RAND_MULTIPLIER * s_ulNextRand) + RAND_INCREMENT;
+	return (int)(s_ulNextRand >> 16U) & 0x7FFFUL;
 }
